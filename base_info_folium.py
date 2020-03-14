ba932dd958a09218033f9b4a953e78008265a933
saeed.moghimi <moghimi@gmail.com>
2020-03-14 15:21:56 -0400

    minorwq

--- base_info_folium.py
+++ base_info_folium.py
@@ -33,89 +33,6 @@ storms = defaultdict(dict)
 
 
 if True:    
-    key  = 'DORIAN'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2019'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
-
-
-
-if True:    
-    key  = 'IRENE'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2011'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   =  -82.50, 12.90, -56.50, 47.30  
-
-if True:    
-    key  = 'SANDY'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2012'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = -82.50, 10.50, -65.50, 41.80
-
-if True:    
-    key  = 'ISAAC'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2012'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
-
-if True:    
-    key  = 'ARTHUR'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2014'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = -82.0, 25.50, -63.50, 47.00
-
-if True:    
-    key  = 'MATTHEW'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2016'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
-
-if True:    
-    key  = 'HERMINE'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2016'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
- 
-if True:    
-    key  = 'IRMA'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2017'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None    
-
-if True:    
-    key  = 'MARIA'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2017'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = -82.50,9.90, -56.50, 44.00
-
-if True:    
-    key  = 'HARVEY'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2017'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = -99.70, 17.90, -88.10, 33.70
-
-
-if True:    
     key  = 'FLORENCE'
     storms[key]['name' ]   = key
     storms[key]['year' ]   = '2018'
@@ -123,21 +40,108 @@ if True:
     storms[key]['end'  ]   = None
     storms[key]['bbox' ]   = None
 
-if True:    
-    key  = 'MICHAEL'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2018'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
 
-if True:    
-    key  = 'BARRY'
-    storms[key]['name' ]   = key
-    storms[key]['year' ]   = '2019'
-    storms[key]['start']   = None 
-    storms[key]['end'  ]   = None
-    storms[key]['bbox' ]   = None
+if False:
+
+    if True:    
+        key  = 'DORIAN'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2019'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
+
+
+
+    if True:    
+        key  = 'IRENE'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2011'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   =  -82.50, 12.90, -56.50, 47.30  
+
+    if True:    
+        key  = 'SANDY'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2012'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = -82.50, 10.50, -65.50, 41.80
+
+    if True:    
+        key  = 'ISAAC'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2012'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
+
+    if True:    
+        key  = 'ARTHUR'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2014'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = -82.0, 25.50, -63.50, 47.00
+
+    if True:    
+        key  = 'MATTHEW'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2016'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
+
+    if True:    
+        key  = 'HERMINE'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2016'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
+     
+    if True:    
+        key  = 'IRMA'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2017'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None    
+
+    if True:    
+        key  = 'MARIA'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2017'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = -82.50,9.90, -56.50, 44.00
+
+    if True:    
+        key  = 'HARVEY'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2017'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = -99.70, 17.90, -88.10, 33.70
+
+
+
+    if True:    
+        key  = 'MICHAEL'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2018'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
+
+    if True:    
+        key  = 'BARRY'
+        storms[key]['name' ]   = key
+        storms[key]['year' ]   = '2019'
+        storms[key]['start']   = None 
+        storms[key]['end'  ]   = None
+        storms[key]['bbox' ]   = None
 
 
 
